0d29d2145d308f97afc752449938588a4069a8a2
Umidjon95 <menumidjonman@gmail.com>
2022-04-14 11:58:28 +0500

    add new interpolation type

--- d8Interpolatsiya.js
+++ d8Interpolatsiya.js
@@ -3,4 +3,5 @@
 const firstName = "Umidjon";
 const age = 26;
 console.log("Mening ismim ", firstName);
+console.log("Mening ismim " + firstName + " yoshim " + age);
 console.log(`Mening ismim ${firstName}. Yoshim ${age}da`);
